4e8a190377515848f7b190e0e7101fff7bba5b21
FernandoDev97 <l.fernando.dev@gmail.com>
2024-01-04 23:53:51 -0300

    finished routes

--- src/controllers/RoomController.ts
+++ src/controllers/RoomController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from 'express'
-import { z } from 'zod'
+import { tuple, z } from 'zod'
 import { roomRepository } from '../repositories/roomRepository'
 import { videoRepository } from '../repositories/videoRepository'
+import { subjectRepository } from '../repositories/subjectRepository'
 
 export class RoomController {
   async create(req: Request, res: Response) {
@@ -59,6 +60,62 @@ export class RoomController {
 
       await videoRepository.save(newVideo)
       return res.status(201).json(newVideo)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error.' })
+    }
+  }
+
+  async roomSubject(req: Request, res: Response) {
+    const videoBodyScrema = z.object({
+      subjectId: z.string(),
+    })
+
+    const videoParamsSchema = z.object({
+      idRoom: z.string(),
+    })
+
+    const { subjectId } = videoBodyScrema.parse(req.body)
+    const { idRoom } = videoParamsSchema.parse(req.params)
+
+    try {
+      const room = await roomRepository.findOne({ where: { id: idRoom } })
+
+      if (!room) {
+        return res.status(404).json({ message: 'Aula nao existe' })
+      }
+
+      const subject = await subjectRepository.findOne({
+        where: { id: subjectId },
+      })
+
+      if (!subject) {
+        return res.status(404).json({ message: 'Disciplina nao existe' })
+      }
+
+      const roomUpdate = {
+        ...room,
+        subjects: [subject],
+      }
+
+      await roomRepository.save(roomUpdate)
+
+      return res.status(200).json(room)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ message: 'Internal server error.' })
+    }
+  }
+
+  async list(_req: Request, res: Response) {
+    try {
+      const rooms = await roomRepository.find({
+        relations: {
+          subjects: true,
+        },
+      })
+
+      return res.status(200).json(rooms)
     } catch (error) {
       console.log(error)
       return res.status(500).json({ message: 'Internal server error.' })
--- src/routes.ts
+++ src/routes.ts
@@ -7,5 +7,7 @@ const routes = Router()
 routes.post('/subject', new SubjectController().create)
 routes.post('/room', new RoomController().create)
 routes.post('/room/:id/create', new RoomController().createVideo)
+routes.post('/room/:idRoom/subject', new RoomController().roomSubject)
+routes.get('/rooms', new RoomController().list)
 
 export default routes
